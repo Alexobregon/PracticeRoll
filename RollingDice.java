6c6d9dc25a559c2a85ea2bd09d9a597304ee1461
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-20 14:21:04 -0800

    added swing imports and panel/frame layout

--- RollingDice.java
+++ RollingDice.java
@@ -1,12 +1,40 @@
-import java.util.Random;
+// import java.util.Random;
+// import java.awt.LayoutManager;
+import java.awt.*;
+// import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class RollingDice {
 
+    public RollingDice() {
+        JFrame frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our Roll");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 public static void main(String[] args) {
 
-    Random rand = new Random();
-    int x = rand.nextInt(6) + 1;
+    new RollingDice();
 
-    System.out.println("You rolled a: " + x);
+    // Random rand = new Random();
+    // int x = rand.nextInt(6) + 1;
+
+    // System.out.println("You rolled a: " + x);
    }
 }
