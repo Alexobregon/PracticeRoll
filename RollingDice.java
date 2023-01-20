75f0d4bd8719e312c9ee774c7de0d92df9cf9cdc
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-20 14:51:54 -0800

    added impements for listener

--- RollingDice.java
+++ RollingDice.java
@@ -1,25 +1,38 @@
 // import java.util.Random;
 // import java.awt.LayoutManager;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 // import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 
-public class RollingDice {
 
-    public RollingDice() {
+public class GUI implements ActionListener {
+	
+	int count = 0;
+	JLabel label;
+	
+	public GUI() {
         JFrame frame = new JFrame();
 
         JButton button = new JButton("Click me");
+        button.addActionListener(this);
+        
+        
+        label = new JLabel("Number of clicks: 0");
 
         JPanel panel = new JPanel();
 
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,14 +40,24 @@ public class RollingDice {
         frame.pack();
         frame.setVisible(true);
     }
+	
+	
+	
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
+	
+		new GUI();
+	}
 
-    new RollingDice();
 
-    // Random rand = new Random();
-    // int x = rand.nextInt(6) + 1;
 
-    // System.out.println("You rolled a: " + x);
-   }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	
+		
+		count++;
+		
+	}
+
 }
